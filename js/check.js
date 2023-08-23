85e72f277f3d5bd5cf390a62788412ff67e3103b
jioh <wldhprincess@naver.com>
2023-08-23 12:57:59 +0900

    인쇄페이지
    인쇄페이지

--- js/check.js
+++ js/check.js
@@ -2,4 +2,8 @@ $(function(){
     $('.blueBtn').click(function(){
         location.href = 'order.html'
     })
+    $('.blackBtn').click(function(){
+        location.href = 'print.html';
+        
+    })
 })
