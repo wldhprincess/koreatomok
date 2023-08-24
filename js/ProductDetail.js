3db96f77ba648af14a50a72d55094dc916d142e2
jioh <wldhprincess@naver.com>
2023-08-24 13:04:11 +0900

    오류 개선, 이미지변경
    오류 개선, 이미지변경

--- js/ProductDetail.js
+++ js/ProductDetail.js
@@ -2,4 +2,7 @@ $(function(){
     $('.margin').click(function(){
         location.href = 'productDetailsPage.html';
     })
+    $('.btnClick').click(function(){
+        location.href = 'productDetailsPage.html';
+    })
 })
