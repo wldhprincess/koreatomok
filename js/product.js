e45c4a2548e631d033f4ebce5182c9413f1ba4f4
wldhprincess <wldhprincess@naver.com>
2023-08-26 17:12:00 +0900

    change ClassName of tag

--- js/product.js
+++ js/product.js
@@ -3,13 +3,13 @@ $(function(){
   var oneDepthIndex = 0;
 
   $('.classifyAShow').show().siblings('a').addClass('selected');
-  $('.classifyMC > li').mouseover(function() {
+  $('.firstDepth > li').mouseover(function() {
       $(this).find('a').addClass('selected');
-      $(this).find('.classifyA').show();
+      $(this).find('.secoudDepth').show();
       
       // 다른 항목의 'selected' 클래스 제거 및 하위 메뉴 숨기기
       $(this).siblings().find('a').removeClass('selected');
-      $(this).siblings().find('.classifyA').hide();
+      $(this).siblings().find('.secoudDepth').hide();
 
       //현재 인덱스 
       oneDepthIndex = $(this).index();
@@ -17,11 +17,11 @@ $(function(){
   $('.productsBtmHide').hide();
   $('.selectItem > ul > li').mouseover(function() {
     $(this).find('a').addClass('selectColor');
-    /* $(this).find('.classifyA').show(); */
+    /* $(this).find('.secoudDepth').show(); */
      
     // 다른 항목의 'selected' 클래스 제거 및 하위 메뉴 숨기기
     $(this).siblings().find('a').removeClass('selectColor');
-    // $(this).siblings().find('.classifyA').hide();/*  */
+    // $(this).siblings().find('.secoudDepth').hide();/*  */
   });
 
   $('.productPhoto1 ').addClass('productPtClick');
@@ -48,38 +48,38 @@ $(function(){
   $('.titleClick1').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow1').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
 
   })
   $('.titleClick2').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow0').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
   })
   $('.titleClick3').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow2').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
   })
   $('.titleClick4').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow3').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
   })
   $('.titleClick5').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow4').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
   })
   $('.titleClick6').mouseover(function(){
     classifyRWraps.hide();
     $('.classifyRBoxShow5').show();
-    $('.classifyA li a').removeClass('select');
+    $('.secoudDepth li a').removeClass('select');
     $(this).next().find('li').eq(0).find('a').addClass('select');
   })
 
@@ -97,7 +97,7 @@ $(function(){
 
   $('.classifyRBoxShow').show();
 
-  const classifyLinks = $('.classifyA a');
+  const classifyLinks = $('.secoudDepth a');
   const classifyRWraps = $('.classifyRBox');
 
   function toggleElement(element) {
@@ -111,8 +111,8 @@ $(function(){
     
     $(this).toggleClass('select');
     
-    const classifyA = $(this).closest('.classifyA');
-    let linkIndex = classifyA.find('a').index(this);
+    const secoudDepth = $(this).closest('.secoudDepth');
+    let linkIndex = secoudDepth.find('a').index(this);
 
     classifyRWraps.hide();
 
@@ -137,7 +137,7 @@ $(function(){
       // 두번째 방법(반복문을 이용하는 방법(최적화))
       /* var total = 0;
       for (var i = 0; i < oneDepthIndex; i++) {
-          var count = $(".classifyA").eq(i).find("li").length;
+          var count = $(".secoudDepth").eq(i).find("li").length;
           total += count;
           console.log("총total " + total);
       } */
